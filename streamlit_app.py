bd687eae093403ad29e9cb22369ef61871d3519b
NehaMarwaha2907 <59535721+NehaMarwaha2907@users.noreply.github.com>
2023-09-08 10:47:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text(' 🐔 🥑🍞Hard-Boiled Free-Range Egg')
+streamlit.text(' 🐔🥑🍞Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
